ecacccfc699ecdae6b56e09c7d7f9ea932c8b41f
saralkumar93 <48145972+saralkumar93@users.noreply.github.com>
2024-11-14 00:22:25 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,7 @@
 #STEP-1: INSTALLING GIT JAVA-1.8.0 MAVEN 
-yum install git java-1.8.0-openjdk maven -y
+#yum install git java-1.8.0-openjdk maven -y
+yum install java-17-openjdk.x86_64 -y
+
 
 #STEP-2: GETTING THE REPO (jenkins.io --> download -- > redhat)
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
